c62f3cbcaed38a7df15803ce17f3fe921ac33433
Samarth Singh Thakur <42175150+hammer221999@users.noreply.github.com>
2019-10-27 20:55:20 +0530

    Update trex.py

--- trex.py
+++ trex.py
@@ -1,18 +1,31 @@
 from mss import mss
 from time import sleep
+import win32gui 
 import pyautogui
-box = (230,480,235,481) #10X35
+box = (230,480,235,481) 
 act = mss()
-sleep(1)
-while True:
-	frame = act.grab(box) 
 
-	if frame.pixel(2,0)[0]<100 and frame.pixel(2,0)[0]>50 or frame.pixel(1,0)[0]<100 and frame.pixel(1,0)[0]>50 or frame.pixel(3,0)[0]<100 and frame.pixel(3,0)[0]>50 :
-		print("jump")
-		pyautogui.press("space")
-		sleep(0.05)        
-	else:
-		pass 
+
+def screen_location():
+	while True:
 	
-	 
+		print(win32gui.GetCursorPos())
+		sleep(1)
+
+
+
+def trex():
+	while True:
+		frame = act.grab(box) 
+
+		if frame.pixel(2,0)[0]<100 and frame.pixel(2,0)[0]>50 or frame.pixel(1,0)[0]<100 and frame.pixel(1,0)[0]>50 or frame.pixel(3,0)[0]<100 and frame.pixel(3,0)[0]>50 :
+			print("jump")
+			pyautogui.press("space")
+			sleep(0.05)        
+		else:
+			pass 
+
+
 		
+		 
+			
